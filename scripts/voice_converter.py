c624430e6e04302fce19f9db5e8ba4244988c984
peisuke <peisuke.com@gmail.com>
2020-05-13 00:38:58 +0900

    fix converter

--- scripts/voice_converter.py
+++ scripts/voice_converter.py
@@ -3,14 +3,47 @@ import time
 import numpy as np
 import threading
 import librosa
+import pyworld
+
+def convert(signal):
+    f0_rate = 2.4
+    sp_rate = 0.8
+    sample_rate = 16000
+
+    f0, t = pyworld.dio(signal, sample_rate)
+    f0 = pyworld.stonemask(signal, f0, t, sample_rate)
+    sp = pyworld.cheaptrick(signal, f0, t, sample_rate)
+    ap = pyworld.d4c(signal, f0, t, sample_rate)
+
+    modified_f0 = f0_rate * f0
+
+    # フォルマントシフト（周波数軸の一様な伸縮）
+    modified_sp = np.zeros_like(sp)
+    sp_range = int(modified_sp.shape[1] * sp_rate)
+    for f in range(modified_sp.shape[1]):
+        if (f < sp_range):
+            if sp_rate >= 1.0:
+                modified_sp[:, f] = sp[:, int(f / sp_rate)]
+            else:
+                modified_sp[:, f] = sp[:, int(sp_rate * f)]
+        else:
+            modified_sp[:, f] = sp[:, f]
+
+    y = pyworld.synthesize(modified_f0, modified_sp, ap, sample_rate)
+
+    return y
 
 class WorkerThread(threading.Thread):
-    def __init__(self):
+    def __init__(self, block_length, margin_length):
         super(WorkerThread, self).__init__()
         self.is_stop = False
         self.lock = threading.Lock()
         self.buffer = []
         self.result = []
+        
+        self.prev_sample = None
+        self.block_length = block_length
+        self.margin_length = margin_length
 
     def stop(self):
         self.is_stop = True
@@ -26,20 +59,37 @@ class WorkerThread(threading.Thread):
                 sample = np.concatenate([b['data'] for b in buf])
 
                 # pitch sift
-                sample = sample.astype(np.float)
-                sample = librosa.effects.pitch_shift(sample.astype(np.float), 16000, n_steps=16)
+                sample = sample.astype(np.float64)
+                sample = convert(sample)
+
+                ###############
+                # overlap
+                current_sample = sample
+
+                if self.prev_sample is None:
+                    sample = current_sample.copy()
+                else:
+                    sample = current_sample.copy()
+                    prev_sample = self.prev_sample
+
+                    chunk_size = sample.shape[0] // self.block_length
+                    margin_size = chunk_size * self.margin_length
+
+                    cw = np.arange(0, 1, 1 / (2*margin_size))
+                    pw = np.ones_like(cw) - cw
+                    len_overlapped = len(cw)
+                    sample[:len_overlapped] = pw * prev_sample[-len_overlapped:] + cw * current_sample[:len_overlapped]
+                    
+                self.prev_sample = current_sample.copy()
+                ########
+            
                 sample = sample.astype(np.int16)
-
-                # fft
-                #orig = sample.copy()
-                #spec = np.fft.rfft(sample)
-                #sample = np.fft.irfft(spec).real.astype(np.int16)
-
-                sample = sample.reshape(16, -1)
+                
+                sample = sample.reshape(self.block_length, -1)
                 buf = [{'data': s, 'index': b['index']} for b, s in zip(buf, sample)]
 
                 with self.lock:
-                    self.result.append(buf[4:-4])
+                    self.result.append(buf[self.margin_length:-self.margin_length])
 
             time.sleep(0.01)
 
@@ -57,7 +107,7 @@ class WorkerThread(threading.Thread):
         return result
 
 class AudioFilter():
-    def __init__(self, worker):
+    def __init__(self, worker, block_length, margin_length):
         self.p = pyaudio.PyAudio()
         input_index, output_index = self.get_channels(self.p)
 
@@ -81,8 +131,13 @@ class AudioFilter():
         self.buffer = []
         self.worker = worker
 
+        self.block_length = block_length
+        self.margin_length = margin_length
+
     def get_channels(self, p):
         input_index = self.p.get_default_input_device_info()['index']
+        output_index = self.p.get_default_output_device_info()['index']
+        output_index = p.get_default_output_device_info()['index']
         for idx in range(self.p.get_device_count()):
             info = self.p.get_device_info_by_index(idx)
             if 'BlackHole' in info['name']:
@@ -94,21 +149,21 @@ class AudioFilter():
 
         self.chunk.append({'data': decoded_data, 'index': self.index})
         self.index += 1
-
+        
         if decoded_data.max() > 1000:
-            self.age = 16 
+            self.age = self.block_length
         else:
             self.age = max(0, self.age - 1)
 
         if self.age == 0:
-            self.chunk = self.chunk[-4:]
+            self.chunk = self.chunk[-self.margin_length:]
         else:
-            if len(self.chunk) >= 16:
+            if len(self.chunk) >= self.block_length:
                 # push self.chunk[0:16]
-                self.worker.push_chunk(self.chunk[0:16])
+                self.worker.push_chunk(self.chunk[0:self.block_length])
 
                 # remove self.chunk[0:8]
-                self.chunk = self.chunk[8:]
+                self.chunk = self.chunk[self.block_length - 2 * self.margin_length:]
 
         # Pop chunk to current list
         ret = self.worker.pop_chunk()
@@ -118,7 +173,6 @@ class AudioFilter():
         # Get head from current list
         if len(self.buffer) > 0:
             ret = self.buffer[0]
-            print(ret['index'], self.index)
             self.buffer = self.buffer[1:]
             data = ret['data']
         else:
@@ -132,11 +186,14 @@ class AudioFilter():
         self.p.terminate()
 
 if __name__ == "__main__":
-    worker_th = WorkerThread()
+    block_length = 4
+    margin_length = 1
+
+    worker_th = WorkerThread(block_length, margin_length)
     worker_th.setDaemon(True)
     worker_th.start()
 
-    af = AudioFilter(worker_th)
+    af = AudioFilter(worker_th, block_length, margin_length)
 
     af.stream.start_stream()
 
